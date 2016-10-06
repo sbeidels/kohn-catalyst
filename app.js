bb473e6548e258668712610cb6c05ee2f79e3046
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-06 19:19:56 -0400

    Added testy functions for mongodb connect and insert

--- app.js
+++ app.js
@@ -47,12 +47,45 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+    res.render('error', {
     message: err.message,
     error: {}
   });
 });
+
+
+/////////////////////////////
+// connect to server
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    db.close();
+});
+
+// insert fake documents
+var insertDocuments = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
+/////////////////////////////
 
 
 module.exports = app;
