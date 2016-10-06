829e6066af1244eb0fb4750cb8c30ccd43f7ce24
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-06 19:37:58 -0400

    Changed mongodb url

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ var MongoClient = require('mongodb').MongoClient
 
 // Connection URL
 // var url = 'mongodb://localhost:27017/myproject';
-var url = 'mongodb://54.69.62.47:27017//?replicaSet=documents';
+var url = 'mongodb://54.69.62.47:27017/?replicaSet=documents';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
