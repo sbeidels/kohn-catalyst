af37aaad4c2fb534be298596b33adebcd944990e
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-06 19:45:45 -0400

    Removed function

--- app.js
+++ app.js
@@ -84,25 +84,7 @@ var insertDocuments = function(db, callback) {
         console.log("Inserted 3 documents into the collection");
         callback(result);
     });
-}
-
-// insert them!!
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
-
-
-
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    insertDocuments(db, function() {
-        db.close();
-    });
-});
+};
 
 
 //
