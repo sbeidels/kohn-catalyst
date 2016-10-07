0af088d76d8fadec91ffbed6e594e23c318f523a
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-06 21:29:20 -0400

    Testing mongoDB to html

---
+++ routes/search-1.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/search-1', function(req, res, next) {
+
+    var query = User.find().limit(10);
+    query.exec(function (err, docs) {
+        if (err) {
+            throw err;
+        }
+        res.render('/search-1', {users: docs});
+    });
+
+
+});
+
+module.exports = router;
