aae8c24446f7bf0559938430f745ef53657e05e8
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-07 09:47:37 -0400

    Examples in the works

--- routes/test.js
+++ routes/test.js
@@ -2,13 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* READ, UPDATE, DELETE */
-router.get('/', function(req, res) {
-    res.send('Show rest list with delete and update feature (CRUD)');
-});
+router.get('/show', function(req, res) {
+    // Get the database
+    var db = req.db;
 
-/* INSERT */
-router.get('/insert_user', function(req, res) {
-    res.send("Insert a new rest (C)");
+    // Fetch the collection 'restaurants'
+    var restCollection = db.get("restaurants");
+
+    // collection.find() returns all items in the db
+    restCollection.find({}, {}, function (err, docs) {
+        assert.equal(err, null);
+        res.render('rest-list', {
+           'restList' : docs
+       });
+    });
+
 });
 
 /* NEW */
@@ -16,9 +24,46 @@ router.get('/new', function(req, res) {
     res.render('rest-new');
 });
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//     res.send('respond with a resource');
-// });
+/* INSERT */
+router.post('/insert_user', function(req, res) {
+    // get the db instance
+    var db = req.db;
+
+    // get POST values from html page
+    var building = req.body.a_building;
+    var coord1 = req.body.a_coord_1;
+    var coord2 = req.body.a_coord_2;
+    var street = req.body.a_street;
+    var zip = req.body.a_zip;
+    var borough = req.body.borough;
+    var cuisine = req.body.cuisine;
+    var name = req.body.name;
+    var restid = req.body.restaurant_id;
+
+    console.log('POST VALUES: ' + '\n' + building + '\n' + coord1 + '\n' + coord2
+        + '\n' + street + '\n' + zip + '\n' + borough + '\n' + cuisine + '\n' +
+        name  + '\n' + restid + '\n');
+
+    var restCollection = db.get("restaurants");
+    restCollection.insert(
+        {
+            "address": {
+                "building": building,
+                "coord": [ coord1, coord2 ],
+                "street": street,
+                "zipcode": zip
+            },
+            "borough": borough,
+            "cuisine": cuisine,
+            "grades": [
+                { "date": { "$date": 1393804800000 }, "grade": "A", "score": -1 },
+            ],
+            "name": name,
+            "restaurant_id": restid
+        }
+    );
+
+    res.send("Insert a new rest (C)");
+});
 
 module.exports = router;
