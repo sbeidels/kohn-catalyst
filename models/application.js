472e347827ada35b95de5ec8b04916416be7482d
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-07 19:24:14 -0400

    First draft of document schema

---
+++ models/application.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ObjectId = Schema.ObjectId;
+
+var applicationSchema = Schema({
+    id:             Number,
+    status:         String,         // TODO: determine concrete review status terms
+    first_name:     String,
+    last_name:      String,
+    dob:            Date,
+    marital_status: String,         // TODO: Confirm options is chosen from list
+    phone_number:   Number,         // TODO: Confirm max length = 10 integers
+    address:        {
+        number:     Number,
+        street:     String,
+        unit:       String,
+        city:       String,
+        state:      String,         // TODO: Validate it is a state or it in Oregon
+        zip:        Number,         // TODO: Confirm max length = 5 integers
+        country:    String          // TODO: Is country needed??
+    },
+    email:          String,         // TODO: Validate in form user@host.host
+    driver_license: String,
+    emergency_contact:  {
+        name:       String,
+        phone:      Number,         // TODO: Validate max length = 10 integers
+    },
+    language:       String,         // TODO: Make user pick from list
+    veteran:        Boolean
+});
---
+++ models/residence.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ObjectId = Schema.ObjectId;
+
+var residenceSchema = Schema({
+    // TODO: The 'id' field references the application.js model HOW?
+    id:                 Number,
+    // TODO: Handle 'other_residents'
+    other_residents:        String,
+    annual_income:          Number,
+    other_assests:          Number,
+    contribute_funds:       Number,
+    family_financial_help:  Number,
+    financial_assistance:   Number,
+    type:                   String,
+    ownership_length:       Number,
+    year_built:             Number,
+    // TODO: Handle requested repairs
+    requested_repairs:      String,
+    heard_about_catalyst:   String,
+    other_non_profits:      String,
+    involved_w_other_org:   String,
+    friends_family_help:    String,
+    property_owner:         String,
+    other_notes:            String
+});
