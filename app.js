afc0105d5c2ecf32cc183a0411c75e252124fb77
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-06 20:08:52 -0400

    Changed mongodb functions

--- app.js
+++ app.js
@@ -56,38 +56,27 @@ app.use(function(err, req, res, next) {
 
 /////////////////////////////
 // connect to server
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://localhost:27017/kohn-catalyst';
+var url = 'mongodb://localhost:27017/test';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-
     db.close();
 });
 
-// insert fake documents
-var insertDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-};
+// Set collection to use
+var collection = db.collection('restaurants');
 
+// Find some documents
+collection.find().toArray(function (err, docs) {
+    test.equal(null, err);
+    console.log(docs);
 
-//
+});
 
 
 /////////////////////////////
