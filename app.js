9d8237ce2abc28dc57883f7a5e3ff518b2d1394e
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-06 21:30:49 -0400

    k

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/search-1', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
