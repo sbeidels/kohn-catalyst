66c1e1e9fcb1d6c466b69be107f81912c568fbe8
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-09 19:14:01 -0400

    test

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('./../mongoose/index');
+var mongoose = require('./../mongoose/index.js');
 // var mongoose = require('mongoose');
 var Application = require('../models/application');
 
