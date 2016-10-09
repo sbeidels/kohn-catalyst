b75949a435e4a58941f7e02b213ae9d3791f7100
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-09 19:10:39 -0400

    undo previous commit

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 // So for every model that you work on you create a model.js document, and
 // add it in the models/index.js
 
-// // Models
-// Application = require('./application');
-// Residence = require('./residence');
-//
-// // Export it so we can use it
-// exports.applicationModel = Application.applicationModel;
-// exports.residenceModel = Residence.residenceModel;
+// Models
+Application = require('./application');
+Residence = require('./residence');
+
+// Export it so we can use it
+exports.applicationModel = Application.applicationModel;
+exports.residenceModel = Residence.residenceModel;
