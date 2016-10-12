9165be5c00402bb95334daf103c94b878d8fdc3b
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-11 23:20:28 -0400

    Updated comments and removed console logs

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,4 +1,3 @@
-// TODO: Create config file
 // Load config file to get URL, username, password, etc
 var config = require('../config');
 
@@ -10,24 +9,22 @@ var mongoose = require('mongoose');
 /* PULL IN SENSITIVE INFORMATION FROM config.js */
 // Local URL, connects EC2 instance to local mongod
 var local_url = 'mongodb://127.0.0.1:27017/test';
+// Connects to the public IP of the server hosting the database
 var remote_url = config.ec2.public_ip;
+// Username and password of a user that has read and write permissions
 var username = config.mongo.username;
 var password = config.mongo.password;
+// Name of the database to use
 var db_name = config.mongo.db.test;
 
-console.log('in mongoose/index.js');
 var options = {
     user: username,
     pass: password
 };
 var uri = 'mongodb://' + remote_url + '/' + db_name;
 
-console.log('after var options mongoose/index.js');
-
 // Connect to the URL
 mongoose.connect(uri, options);
-
-console.log('after mongoose.connect mongoose/index.js');
 
 // Nothing needs to be exported, simple use "  require('<path>/mongoose')  "
 module.exports = mongoose;
