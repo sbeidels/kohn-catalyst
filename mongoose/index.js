31528244bb3401a90e82436ef963568f01865f68
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-17 19:36:00 -0400

    Added logging

--- mongoose/index.js
+++ mongoose/index.js
@@ -25,7 +25,7 @@ var uri = 'mongodb://' + remote_url + '/' + db_name;
 mongoose.connect(uri, options);
 mongoose.connection.on('error', console.error.bind(console, 'Connection Error : '));
 mongoose.connection.once('open', function () {
-    console.log('Connection to database ok!');
+    console.log('[ DATABASE ] Connection to database ok!');
 });
 
 // Nothing needs to be exported, simple use "  require('<path>/mongoose')  "
