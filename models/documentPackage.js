f9dc15090a45fb32beaa8fc1d35268186a975dbc
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-21 10:04:10 -0400

    Creation of the entire document, application portion complete\n\nThis schema, when complete, will hold all data related to the client, their property to be repaired, and the project documentation.

---
+++ models/documentPackage.js
@@ -0,0 +1,90 @@
+/*
+ This file represents the doument schema for the entire DOCUMENT PACKAGE
+ This is the master file for a client. Imagine it as a section in a filing
+ cabinet that holds the application, financial, property, handle-it and
+ project information. A 'folder' to hold a bunch of forms.
+
+ This schema will create a mongoDB 'document'.
+ The document will have values, like '_id', and sub-documents,
+ like 'applications'.
+ A sub-document can have sub-documents or values.
+
+ Items on the left of the colon are database ID values.
+ Items on the right of the colon are the data types allowed.
+
+ Allowable types are:
+     String      Number
+     Date        Buffer
+     Boolean     Mixed
+     ObjectId    Array
+
+ ObjectId is similar to a primary key in a relationship based database.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+ObjectId = Schema.ObjectId;
+
+// TODO: Add access tags as needed - docs = https://docs.mongodb.com/manual/reference/operator/aggregation/redact/
+var DocumentPackageSchema = new Schema({
+    _id:            ObjectId,
+    status:         String,             // TODO: determine concrete review status terms
+
+    advocate:       {
+        is_advocate:    Boolean,
+        name:           String,
+        relationship:   String,
+        phone:          Number,
+        non_profit_name:String
+    },
+
+    application:    {
+        owns_home:      Boolean,        // TODO: is this still asked/on the form?
+        first_name:     String,
+        last_name:      String,
+        dob:            Date,           // TODO: split into subdocument and add security tags
+        driver_license: String,         // TODO: split into subdocument and add security tags
+        is_married:     Boolean,
+        spouse:         String,
+        phone:          {
+            home:       Number,         // TODO: Validate max length = 10 integers
+            cell:       Number,         // TODO: Validate max length = 10 integers
+        },
+        email:          String,         // TODO: Validate in form user@host.host
+        address:        {
+            line_1:     String,
+            line_2:     String,
+            city:       String,
+            state:      String,         // TODO: Validate it is a state or it is in Oregon
+            zip:        Number,         // TODO: Confirm max length = 5 integers
+        },
+        emergency_contact:  {
+            name:           String,
+            relationship:   String,
+            phone:          Number      // TODO: Validate max length = 10 integers
+        },
+        other_residents:{
+            number:      Number,        // other_residents.names[index], other_residents.ages[index] where index < other_residents.number
+            names:      [String],       // This is an array of strings
+            ages:       [Number]        // This is an array of strings
+        },
+        veteran:        Boolean,
+        language:       String,
+        heard_about:    String
+    },
+
+    finance:    {
+
+    },
+
+    property:   {
+
+    },
+
+    project:    {
+
+    }
+});
+
+var DocumentPackage = mongoose.model('DocumentPackage', DocumentPackageSchema);
+module.exports = DocumentPackage;
