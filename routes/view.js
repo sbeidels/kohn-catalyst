6c795ba9611a582871533b691c00e9beb875d17a
FearJoy <john.fitzpatrick.v@gmail.com>
2016-10-25 23:58:23 -0400

    Added example of how to use GET request API. Learn well

--- routes/view.js
+++ routes/view.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var mongoose = require('mongoose');
 // var db = require('../mongoose/index');
 var Application = require('../models/application');
+var DocumentPackage = require('../models/documentPackage');
+var api = require('../controllers/api');
+
 
 var Promise = require('bluebird'); // Import promise engine
 mongoose.Promise = require('bluebird'); // Tell mongoose we are using the Bluebird promise library
@@ -29,6 +32,63 @@ router.get('/', function(req, res) {
         });
 
 });
+
+/** This is the beginning of an example to use
+ *          getDocumentByStatus
+ */
+router.get('/status', api.getDocumentByStatus, function(req, res, next) {
+    if (res.locals.results.new[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.new array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.phone[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.phone array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.documents[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.documents array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.discuss[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.discuss array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.visit[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.visit array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.handle[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.handle array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.declined[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.declined array is empty');
+    } else {
+        // Do something with the documents you found
+    }
+
+    if (res.locals.results.project[0] == null) {
+        console.log('[ ROUTER ] /view/status :: res.locals.results.project array is empty');
+    } else {
+
+    }
+    res.json(res.locals.results);
+});
+/** This is the end of an example to use
+ *          getDocumentByStatus
+ */
 
 /* Route to specific application */
 router.get('/:id', function(req, res) {
