6317efdf5e5bed2163022e08735ed09ef93ff53f
ironmikesh <hainemic@oregonstate.edu>
2016-10-28 16:32:51 -0700

    Added some testing files
    form.hbs is the json testing form html stuff
    addForm.js has been changed to the JSON testing handler

--- routes/addForm.js
+++ routes/addForm.js
@@ -1,89 +1,103 @@
-$("#appForm").submit(function (event) {
-    // Get values from elements on the page:
-    var $form = $(this);
-    var json = JSON.stringify( $appForm.serializeArray() );
-    
-    // Send the data using post
-    var <variable you send to database> = $.post(url, json);
-});
+$(document).ready(init)
 
 
-/*
-advocate *bool
-advocate-name
-advocate-relationship
-advocate-npoPhone
-advocate-npo
-firstName
-middleName
-lastName
-preferredName
-dob //date
-driversLicense 
-marriedSingle  //(text from radio - Married or Single are the two values)
-spouse
-hPhone
-cPhone
-emailaddy  //(email type, but I think that's just text with some constraints in form handling)
-add1
-add2
-city
-state
-zip
-eContactName
-ecRelationship
-ecPhone
-additional-1
-a1age //number
-a1relationship
-additional-2
-a2age //number
-a2relationship
-additional-3
-a3age //number
-a3relationship
-additional-4
-a4age //number
-a4relationship
-additional-5
-a5age //number
-a5relationship
-military //bool y/n
-language (text from radio - English or Other)
-language-other
-hearAboutCatalyst
-monthlyMortgage (number)
-currentOnMortgage (bool)
-annualIncome (number)
-assets1
-assets1-value (number)
-assets2
-assets2-value  (number)
-assets3
-assets3-value  (number)
-assets4
-assets4-value  (number)
-assets5
-assets5-value  (number)
-contribute  (bool)
-contribute-amount  (number)
-relativeHelp (bool)
-relativeHelp-provide
-otherHelp  (bool)
-otherHelp-provide
-propertyType (text from radio)
-propertyType-other
-timePropertyOwned
-yearPropertyBuilt (number)
-repairsNeeded
-help  (bool)
-help-personal 
-othersHelp (bool)
-others-help
-fbo (bool)
-fbo-name
-fbo-help (bool)
-otherCircumstances
-tac-yes (bool)
-signature
-*/
+function init() {
+
+    $('#testButton').on('click', getFormDataJson);
+
+    /*
+
+    This is a dumb/blind way to just rip through all the form's inputs and get their current values.
+    This will return an object with key/value pairs where the key is the `input.name`.
+    So if your form's DOM input names/structure is setup in a way that directly matches the service contract,
+    you're in good shape.
+
+    But I think this approach may not be wise, since you'll want to do things like have an `address` object
+    with it's own properties, or in other words your form is complex enough that you can't do it in a dumb way.
+
+    */
+    function getFormDataJson() {
+        var serialized = $('#appForm').serializeArray();
+        var data = {};
+
+        console.log('-------------------------------------');
+        console.debug('Serializing form data as JSON... found %s fields in form.', serialized.length);
+
+        serialized.forEach( function(inputData) {
+            console.log('\t:: ' + inputData.name + ' = ' + inputData.value);
+            data[inputData.name] = inputData.value;
+
+        });
+
+        var json = JSON.stringify(data);
+
+        console.log('-------------------------------------');
+        console.log('data: ', data);
+        console.log('data as json: ', json);
+        console.log('-------------------------------------');
+
+        return json;
+    }
+
+    /*
+
+    Alternate approach below, where you could start with a blank object `data`, and by
+    using jQuery's `extend` method, append/add all the indvidual form sections to one master object
+    that can be sent to the server as JSON.
+
+    In this way, you can do whatever weird custom shit you may need to do for formatting/extracting data,
+    but do it within the smaller/more manageable functions below.
+
+    It also does not make sense to create individual variables for all the values. Stick that shit
+    right onto an object, if you find yourself wanting to do `var firstName = $(...)`, you're probably doing it wrong.
+
+    */
+
+    function getApplicationFormJSON() {
+        var data = {};
+
+        $.extend(data, getPersonalInfoData());
+        $.extend(data, getFinancialInfoData());
+        $.extend(data, getPropertyInfoData());
+
+        return JSON.stringify(data);
+    }
+
+    function getPersonalInfoData() {
+        var data = {};
+
+        // This is where you'd tack the form values in the Personal Info section onto `data`
+        data.firstName = getVal('input[name="firstName"]');
+        data.lastName = getVal('input[name="lastName"]');
+        data.address = {
+            line1: getVal('input[name="address.line1"]'),
+            line2: getVal('input[name="address.line2"]'),
+            city: getVal('input[name="address.city"]'),
+            state: getVal('input[name="address.state"]'),
+            zip: getVal('input[name="address.zip"]')
+        };
+
+        return data;
+    }
+
+    function getFinancialInfoData() {
+        var data = {};
+
+        // This is where you'd tack the form values in the Personal Info section onto `data`
+
+        return data;
+    }
+
+    function getPropertyInfoData() {
+        var data = {};
+
+        // This is where you'd tack the form values in the Personal Info section onto `data`
+
+        return data;
+    }
+
+    function getVal(selector) {
+        return $(selector).val();
+    }
+
+}
