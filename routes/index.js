80c9bac8349205f0831762527baded4b143bbcac
FearJoy <john.fitzpatrick.v@gmail.com>
2016-11-08 14:54:56 -0500

    Add brief example code to render text. Remove old text.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Welcome to kohn-catalyst' });
+    res.render('index', {
+        title: 'I am a title property from index.js',
+        body: 'I am a body property in route \'/\''
+    });
 });
 
 module.exports = router;
