8d1460cefc4ce833730b9cd826044be841dda497
ironmikesh <hainemic@oregonstate.edu>
2016-11-14 17:19:11 -0800

    If Yes TESTED and Functioning Properly

--- public/javascripts/noalert.js
+++ public/javascripts/noalert.js
@@ -1,18 +1,23 @@
-$('#alertOwn').on('click', alertOwnFunction);
-$('#alertMortgage').on('click', alertMortgageFunction);
+$(document).ready(init)
 
-function alertOwnFunction() {
-	if (getVal('input[name="owns_home"]:checked')) {
-		alert("We are so sorry, but Catalyst does not provide services for properties that applicants do not own.");
+function init() {
+
+	$('#alertOwn').on('click', alertOwnFunction);
+	$('#alertMortgage').on('click', alertMortgageFunction);
+
+	function alertOwnFunction() {
+		if (getVal('input[name="owns_home"]:checked')) {
+			alert("We are so sorry, but Catalyst does not provide services for properties that applicants do not own.");
+		}
 	}
-}
 
-function alertMortgageFunction() {
-	if (getVal('input[name="mortgage_up_to_date"]:checked')) {
-		alert("We are so sorry, but if you are not up to date on your mortgage payments, Catalyst cannot provide services for your property.");
+	function alertMortgageFunction() {
+		if (getVal('input[name="mortgage_up_to_date"]:checked')) {
+			alert("We are so sorry, but if you are not up to date on your mortgage payments, Catalyst cannot provide services for your property.");
+		}
 	}
-}
 
-function getVal(selector) {
-		return $(selector).val();
+	function getVal(selector) {
+			return $(selector).val();
+	}
 }
