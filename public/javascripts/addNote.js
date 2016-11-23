49208268fb51239fe8b0298af9f479e04c718e28
Marco Zamora <marco.zamora@comcast.net>
2016-11-22 22:06:15 -0600

    vetting notes create/delete framework

--- public/javascripts/addNote.js
+++ public/javascripts/addNote.js
@@ -1,14 +1,41 @@
-$(document).ready(init)
+/**
+ * This script handles adding and removing of vetting notes
+**/
+
+$(document).ready(init);
 
 function init() {
 
-    $('#addNote').on('click', addTheNote);
-	function addTheNote() {
-		//addTheNote.preventDefault();
-        var note = $('#note').val();
-        console.log("Adding note: " + note);
-		//TODO: submit to DB once DB is up and running
-	}
-}
-	    
+    $('#addNote').on('click', addNote);
+    //since delete buttons are dynamically add, the click listener is on the table body
+    $('#notes-body').on('click', '.delete-button', deleteNote);
 
+}
+
+function addNote(e) {
+    e.preventDefault();
+    //TODO: submit to DB once DB is up and running
+    //append new row with delete button
+    if($('#note').val() != "") {
+        var newRow = '<tr><td>' + getDate() + '</td>' + '<td>' + $('#note').val() + '</td><td><button type="submit" class="delete-button btn btn-danger">Delete Note</button></td></tr>';
+        $('#notes-body tr:last').before(newRow);
+        //clear value
+        $('#note').val("");
+    }
+}
+
+function deleteNote(e) {
+    e.preventDefault();
+    //TODO: submit to DB once DB is up and running
+    //remove row
+    $(this).closest('tr').remove();
+}
+
+//get date in a nice format
+function getDate() {
+    var date = new Date();
+    var Year = date.getFullYear();
+    var Day = ( "00" + date.getDate()).slice(-2);
+    var Mon = ("00" + (date.getMonth()+1)).slice(-2);
+    return Mon + "/" + Day + "/" + Year;
+}
