922a2fbc49cd0439b95531c58a70216af693f67d
FearJoy <john.fitzpatrick.v@gmail.com>
2016-11-25 16:35:40 -0500

    Add route to handle updating highlightPackage on db

--- routes/edit.js
+++ routes/edit.js
@@ -18,6 +18,20 @@ router.post('/', function(req, res) {
 });
 
 /**
+    edit highlight features
+ **/
+router.post('/highlight/:id', api.toggleHighlight, function(req, res) {
+
+    if(res.locals.status != '200'){
+        res.status(500).send("Could not update field");
+    }
+    else{
+        res.status(200).send({ status: 'success' });
+    }
+
+})
+
+/**
  * Get Updated Data
  * Most fields can now be updated using the API
  **/
