0ee1e3a172b7a26ddf66ec74861d28dfeb826599
Marco Zamora <marco.zamora@comcast.net>
2016-11-30 21:35:18 -0600

    more comments on my public JS files

--- public/javascripts/addNote.js
+++ public/javascripts/addNote.js
@@ -1,5 +1,6 @@
 /**
  * This script handles adding and removing of vetting notes
+ * It works in both the application view and the phone call script
 **/
 
 $(document).ready(init);
@@ -7,7 +8,8 @@ $(document).ready(init);
 function init() {
 
     $('#addNote').on('click', addNote);
-    //since delete/update buttons are dynamically added, the click listener is on the table body
+    //since delete/update buttons are dynamically added, we use the table body ID to apply click listeners
+    //the second parameter is the class name the click listener is applied to
     $('#notes-body').on('click', '.delete-button', deleteNote);
     $('#notes-body').on('click', '.update-button', setUpForm);
     $('#notes-body').on('click', '.submit-update-button', updateNote);
@@ -15,12 +17,17 @@ function init() {
 
 }
 
+/**
+ * When the add button is clicked, the following function is called
+ *
+ */
 function addNote(e) {
-    e.preventDefault(); //don't refresh page/POST
+    e.preventDefault(); //prevent page refresh/POST to this page
 
-    //append new row with delete button
+    //don't bother doing anything if text field was empty
     if($('#note').val() != "") {
 
+        //otherwise, prepare payload with the application ID and the note contents
         var payload = {};
         payload.description = $('#note').val();
         payload.applicationId = $('#appId').val();
@@ -35,7 +42,7 @@ function addNote(e) {
         });
         posting.done(function (xhr) {
             if(xhr.status == 200) {
-                //check if the empty notes row exists and delete if so
+                //check if the 'empty notes' row exists and delete if so
                 var emptyNotes = $('#empty-notes');
                 if(emptyNotes != null)
                 {
@@ -43,35 +50,47 @@ function addNote(e) {
                 }
 
                 //build the new row
+                //the date column to display
                 var date = '<td>' + getDate() + '</td>';
+                //new column for the new note
                 var newNote = '<td>' + $('#note').val() + '</td>';
+                //need to get the new note ID so it is available for immediate update/deletion
                 var hiddenNoteId = '<input type="hidden" value="' + xhr.noteId + '" name="noteId" />';
+                //build delete button
                 var deleteButton = '<button type="submit" class="delete-button btn btn-danger">Delete Note</button>';
+                //assemble all parts to build the new note row
                 var newRow = '<tr class="success">' + date + newNote + '<td><form>' + hiddenNoteId + deleteButton + '</form></td></tr>';
                 //add new row before the very last row in the table (input form row)
                 $('#notes-body tr:last').before(newRow);
-                //clear value
+                //clear text area to prepare for new note
                 $('#note').val("");
             }
             else{
-                console.log("Whoops, something went wrong");
+                console.log("API submission for new note failed");
             }
             // If code is not 200 forward below to .fail()
         });
 
         posting.fail(function (data)
         {
-            console.log("Whoops, something went wrong");
-            // The save failed, just do nothing and leave the form without losing their typed data
+            console.log("ajax failed to POST data");
         });
 
     }
 }
 
+/**
+ * When the delete button is clicked, the following function is called
+ *
+ */
 function deleteNote(e) {
-    e.preventDefault();
+    e.preventDefault(); //prevent page refresh/POST
+    //get the note ID from the nearest hidden input field
     var noteId = $(this).closest("form").find("input[name='noteId']").val();
+    //save note row object in variable
     var noteSelected = $(this);
+
+    //payload needs the note id so it can be found and removed
     var payload = {};
     payload.noteId = noteId;
 
@@ -84,34 +103,35 @@ function deleteNote(e) {
     });
     posting.done(function (xhr) {
         if(xhr.status == 200) {
-            //remove row
+            //remove row on successful document deletion
             noteSelected.closest('tr').remove();
         }
         else{
-            console.log("Did not receive 200 status for deleting note");
+            console.log("API did not return 200 status for deleting note");
         }
     });
     posting.fail(function (data)
     {
-        console.log("Whoops, something went wrong");
+        console.log("Ajax POST failed");
     });
 
 }
 
 /**
  * Sets up the html for editing
+ * When the Update Note button is clicked, the form needs to transform to include new buttons and functions
  */
 function setUpForm(e) {
     e.preventDefault();
-    //the note we want will be the first child of the following search
+    //the note we want will be the first child of the following search since it finds the closest <tr> to the button clicked
     var note = $(this).closest("tr").find(".note-descrip");
-    //make the text into a text area for editing
+    //transform the text into a text area for editing
     note.html('<textarea id="note" class="form-control note-textarea" placeholder="Update Note Here...">' + note[0].innerText + '</textarea>');
 
-    //change update note button to submit change button
+    //change update note button to submit change button that will handle update submission
     $(this).val("Submit Update");
     $(this).attr("class", "submit-update-button btn btn-warning");
-    //change delete button to cancel button
+    //change delete button to cancel button to handle cancellation of updates
     var cancelButton = $(this).closest("td").find(".delete-button")
     cancelButton.val("Cancel");
     cancelButton.attr("class", "cancel-button btn btn-danger");
@@ -120,13 +140,14 @@ function setUpForm(e) {
 function updateNote(e) {
     e.preventDefault();
     //save items for after POST
-    var note = $(this).closest("tr").find(".note-descrip");
-    var updateButton = $(this);
-    var cancelButton = $(this).closest("td").find(".cancel-button");
-    var noteId = $(this).closest("form").find("input[name='noteId']").val();
-    //using .value since innerHTML and other don't get updated
+    var note = $(this).closest("tr").find(".note-descrip");                     //the note row html object
+    var updateButton = $(this);                                                 //associated update button
+    var cancelButton = $(this).closest("td").find(".cancel-button");            //associated cancel button
+    var noteId = $(this).closest("form").find("input[name='noteId']").val();    //associated note ID
+    //using .value since innerHTML and others don't get updated when textarea is edited
     var notedDescrip = $(this).closest("tr").find(".note-textarea")[0].value;
 
+    //build payload with the note idea and new note description
     var payload = {};
     payload.description = notedDescrip;
     payload.id = noteId;
@@ -141,27 +162,30 @@ function updateNote(e) {
     });
     posting.done(function (xhr) {
         if(xhr.status == 200) {
-            //revert form into just text
+            //revert form into just text after success
             note[0].innerHTML = notedDescrip;
-            //revert buttons
+            //revert buttonsby changing button value and class
             updateButton.val("Update Note");
             updateButton.attr("class", "update-button btn btn-info");
             cancelButton.val("Delete Note");
             cancelButton.attr("class", "delete-button btn btn-danger");
         }
         else{
-            console.log("Whoops, something went wrong");
+            console.log("API did not return 200 status for updating note");
         }
         // If code is not 200 forward below to .fail()
     });
 
     posting.fail(function (data)
     {
-        console.log("Whoops, something went wrong");
-        // The save failed, just do nothing and leave the form without losing their typed data
+        console.log("Ajax POST failed");
     });
 }
 
+/**
+ * If cancel button is clicked, undo the changes made by setup form and revert buttons and their class names
+ *
+ */
 function cancelUpdate(e){
     e.preventDefault();
     var note = $(this).closest("tr").find(".note-descrip");
@@ -178,7 +202,7 @@ function cancelUpdate(e){
     cancelButton.attr("class", "delete-button btn btn-danger");
 }
 
-//get date in a nice format
+//helper function to get date in a nice format
 function getDate() {
     var date = new Date();
     var Year = date.getFullYear();
