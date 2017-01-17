cc131a79c1fe8d150248d027870c37965c0459a9
FearJoy <john.fitzpatrick.v@gmail.com>
2017-01-17 10:11:03 -0500

    Restructure connection URI

--- mongoose/connection.js
+++ mongoose/connection.js
@@ -6,14 +6,27 @@ var mongoose = require('mongoose');     // Import the mongoose module
 var local_url = 'mongodb://127.0.0.1:27017/test';   // Local URL, connects EC2 instance to local mongod
 var remote_url = config.ec2.public_ip;              // Connects to the public IP of the server hosting the database
 
-var options = {
-    username:   config.mongo.username,      // Username and password of a user that has read and write permissions
-    password:   config.mongo.password
-};
-var uri = 'mongodb://' + remote_url + '/' + config.mongo.db;
+// var options = {
+//     username:   config.mongo.username,      // Username and password of a user that has read and write permissions
+//     password:   config.mongo.password
+// };
+
+// Example: mongodb://username:password@127.0.0.1:27017/database-to-use?authSource=admin
+var uri = 'mongodb://'
+    + config.mongo.username
+    + ':'
+    + config.mongo.password
+    + '@'
+    + config.ec2.public_ip
+    + ':'
+    + '27017'
+    + '/'
+    + config.mongo.db
+    + '?authSource=admin';
 
 // Connect to the URL
-mongoose.connect(uri, options);
+// mongoose.connect(uri, options);
+mongoose.connect(uri);
 mongoose.connection.on('error', console.error.bind(console, '[ DATABASE ] Connection :: Connection response: '));
 mongoose.connection.once('open', function () {
     console.log('[ DATABASE ] Connection :: Successfully connected to the database: ' + config.mongo.db);
