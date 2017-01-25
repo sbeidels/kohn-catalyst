2001fff2483d5d04bc360a0d863a034f99fe429c
sbeidels <beidelss@oregonstate.edu>
2017-01-24 21:23:17 -0500

    signature and timestamp saved

--- models/documentPackage.js
+++ models/documentPackage.js
@@ -166,6 +166,12 @@ var DocumentPackageSchema = new Schema({
         fbo_name: String
     },
 	
+	signature:    {
+		client_terms: Boolean,
+		client_sig: String,
+		client_date: { type: Date, default: Date.now}
+	},
+	
 	// Note: upon application submission, the Yes checkbox (name="tac-yes") and the digital signature (name="signature") at the bottom of the form are not captured anywhere.
 
     project:    {
